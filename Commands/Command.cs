793ddfcddd8b37620bc2f5bc48d06faab2a40bb5
Coolpuppy24 <pat.pvp.unturned@gmail.com>
2017-04-28 21:27:22 -0500

    Plugin Created!

---
+++ Commands/Command.cs
@@ -0,0 +1,79 @@
+﻿using Rocket.API;
+using Rocket.API.Extensions;
+using Rocket.Core.Logging;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace coolpuppy24.staffhelp
+{
+    public class Command : IRocketCommand
+    {
+        public static Main Instance;
+
+        public List<string> Aliases
+        {
+            get
+            {
+                return new List<string>() {"sh"};
+            }
+        }
+
+        public AllowedCaller AllowedCaller
+        {
+            get
+            {
+                return AllowedCaller.Player;
+            }
+        }
+
+        public string Help
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "staffhelp";
+            }
+        }
+
+        public List<string> Permissions
+        {
+            get
+            {
+                return new List<string>() { "staffhelp.send" };
+            }
+        }
+
+        public string Syntax
+        {
+            get
+            {
+                return "<message>";
+            }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+
+            string message = command.GetParameterString(0);
+
+            foreach (UnturnedPlayer staffmems in Main.Instance.Players())
+            {
+                if (staffmems.HasPermission("staffhelp.receive"))
+                {
+                    UnturnedChat.Say(staffmems, "[StaffHelp] " + player.CharacterName + ": " + message, UnturnedChat.GetColorFromName(Main.Instance.Configuration.Instance.ChatColorName, Color.red));
+                }
+            }
+        }
+    }
+}
---
+++ Configuration.cs
@@ -0,0 +1,19 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace coolpuppy24.staffhelp
+{
+
+    public class Configuration : IRocketPluginConfiguration
+    {
+        public string ChatColorName;
+
+        public void LoadDefaults()
+        {
+            ChatColorName = "Blue";
+        }
+    }
+}

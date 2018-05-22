0ef97380e5d5afc85a4da5ab2f812a113bccf872
Trojaner_ <trojaner25@gmail.com>
2018-05-22 14:42:45 +0300

    RocketMod 5.

---
+++ Commands/CommandStaffHelp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Rocket.API.Commands;
+using Rocket.API.Permissions;
+using Rocket.API.User;
+using Rocket.Core.User;
+
+namespace StaffHelp.Commands
+{
+    public class CommandStaffHelp : ICommand
+    {
+        public string Name => "StaffHelp";
+        public string[] Aliases => new[] {"sh"};
+        public string Summary => "Sends message to staff.";
+        public string Description => null;
+        public string Permission => null;
+        public string Syntax => "<message>";
+        public IChildCommand[] ChildCommands => null;
+
+        public bool SupportsUser(Type user)
+        {
+            return true;
+        }
+
+        public void Execute(ICommandContext context)
+        {
+            var userManager = context.Container.Resolve<IUserManager>();
+            var permissionManager = context.Container.Resolve<IPermissionProvider>();
+
+            string message = string.Join(" ", context.Parameters.ToArray());
+
+            List<IUser> receivers = new List<IUser>();
+            foreach (var user in userManager.Users)
+            {
+                if(permissionManager.CheckPermission(user, "staffhelp.receive") != PermissionResult.Grant)
+                    continue;
+                receivers.Add(user);
+            }
+
+            userManager.Broadcast(context.User, receivers, $"[StaffHelp] {context.User.Name}: {message}", Color.Red);
+        }
+    }
+}
---
+++ StaffHelpPlugin.cs
@@ -0,0 +1,12 @@
+﻿using Rocket.API.DependencyInjection;
+using Rocket.Core.Plugins;
+
+namespace StaffHelp
+{
+    public class StaffHelpPlugin : Plugin
+    {
+        public StaffHelpPlugin(IDependencyContainer container) : base("StaffHelp", container)
+        {
+        }
+    }
+}

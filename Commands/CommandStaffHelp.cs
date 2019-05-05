52b81f090e122abc186e988d45c0de94b61f638b
archie426 <archiestride42@gmail.com>
2019-05-05 11:41:09 +0100

    Finish updating

--- Commands/CommandStaffHelp.cs
+++ Commands/CommandStaffHelp.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Threading.Tasks;
 using Rocket.API.Commands;
 using Rocket.API.Permissions;
+using Rocket.API.Player;
 using Rocket.API.User;
-using Rocket.Core.User;
+using Rocket.Unturned.Player;
+
 
 namespace StaffHelp.Commands
 {
@@ -24,17 +25,17 @@ namespace StaffHelp.Commands
         public async Task ExecuteAsync(ICommandContext context)
         {
             var userManager = context.Container.Resolve<IUserManager>();
-            var permissionManager = context.Container.Resolve<IPermissionProvider>();
-            var permissionChecker = context.Container.Resolve<IPermissionChecker>;
+            var permissionChecker = context.Container.Resolve<IPermissionChecker>();
+            var player = context.Container.Resolve<IPlayer>();
 
             string message = string.Join(" ", context.Parameters.ToArray());
 
             List<IUser> receivers = new List<IUser>();
-            foreach (var user in userManager.)
+            foreach (var user in await player.PlayerManager.GetPlayersAsync())
             {
-                if(permissionChecker.CheckPermission(user, "staffhelp.receive") != PermissionResult.Grant)
+                if(await permissionChecker.CheckPermissionAsync(user.User, "staffhelp.receive") != PermissionResult.Grant)
                     continue;
-                receivers.Add(user);
+                receivers.Add(user.User);
             }
 
             await userManager.BroadcastAsync(context.User, receivers, $"[StaffHelp] {context.User.UserName}: {message}", Color.Red);
